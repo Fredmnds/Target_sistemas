ae4a549d54541924a71bdee0d6fbef0050d7ef8c
Fredmnds <fredmendesdc@gmail.com>
2024-09-02 15:34:49 -0300

    Subindo exercicios

---
+++ ExercicioFibonacci/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int number = 6763;
+
+        if (IsFibonacci(number))
+        {
+            Console.WriteLine($"{number} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} não pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool IsFibonacci(int number)
+    {
+        if (number < 0)
+        {
+            return false;
+        }
+
+        int a = 0, b = 1;
+
+        if (number == a || number == b)
+        {
+            return true;
+        }
+
+        int c = a + b;
+
+        while (c <= number)
+        {
+            if (c == number)
+            {
+                return true;
+            }
+
+            a = b;
+            b = c;
+            c = a + b;
+        }
+
+        return false;
+    }
+}
---
+++ ExercicioString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string phrase = "Rato roeu a roupa do rei de Roma";
+        CountLetterA(phrase);
+    }
+
+    static void CountLetterA(string phrase)
+    {
+        int count = 0;
+
+        foreach (char character in phrase)
+        {
+            if (character == 'a' || character == 'A')
+            {
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine("Não há a presença do 'a' nessa palavra");
+        }
+        else
+        {
+            Console.WriteLine($"Apareceu {count} vezes");
+        }
+    }
+
+}
